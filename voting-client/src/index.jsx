2a1585235fc6892a6cd3d76358d6148d47a27dcd
Andre Pitombeira <andrepitombeira@gmail.com>
2015-10-26 15:25:45 +0100

    Create reducer on client

--- voting-client/src/index.jsx
+++ voting-client/src/index.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
 import ReactDom from  'react-dom';
+import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
-const pair = ['Trainspotting', '28 Days Later'];
+const store = createStore(reducer);
+
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['Sunshine', '28 Days Later'],
+      tally: {Sunshine: 2}
+    }
+  }
+});
+
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
+  <Route path="/" component={Voting} />
+</Route>;
 
 ReactDom.render(
-	<Voting pair={pair} winner="Trainspotting" />,
-	document.getElementById('app')
+	<Router>{routes}</Router>,
+		document.getElementById('app')
 );
